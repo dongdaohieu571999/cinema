03015dea9db56be6bcf9c866f034d1615f390232
Nomm <94917416+PhiHung151@users.noreply.github.com>
2023-03-31 00:36:05 +0700

    commit 1 31/3
    update giao diện

---
+++ resources/interface.blade.php
@@ -0,0 +1,18 @@
+<!-- <div class="container mobile-app">
+           <div class="mobile-content col-xl-6">
+               <div class="mobile-content-text">
+                   <h1>Download our mobile apps</h1>
+                   <div class="decoration">
+                       <div class="decoration-text"></div>
+                   </div>
+                   <p>Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely</p>
+                   <button>
+                       <a href="#">Download <i class="fa-solid fa-arrow-down"></i></a>
+                   </button>
+               </div>
+           </div>
+          
+           <div class="mobile-img col-xl-6">
+               <img src="./img/trafalgar-illustration%20sec03%201.jpg" alt="">
+           </div>
+        </div> -->
